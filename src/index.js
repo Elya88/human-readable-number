a26681647b5da4695fa2f3956b594f33bfc9b6d4
Elnura <elnura.sarkobenova.kk@gmail.com>
2022-02-11 19:16:38 +0100

    feat: node

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function humanReadable(second) {
+    const humanReadable = seconds =>
+        [
+            ('0' + parseInt(secont / (60 * 60))).slice(-2),
+            ('0' + parseInt(secont / 60 % 60)).slice(-2),
+            ('0' + second % 60).slice(-2)
+            
+      ].join(':')
 }
