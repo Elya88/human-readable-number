5b476a5be5f8efa7d333e11056fc050b78415196
Elnura <elnura.sarkobenova.kk@gmail.com>
2022-02-15 19:02:28 +0100

    feat: add index

--- src/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function humanReadable(second) {
-    const humanReadable = seconds =>
-        [
-            ('0' + parseInt(secont / (60 * 60))).slice(-2),
-            ('0' + parseInt(secont / 60 % 60)).slice(-2),
-            ('0' + second % 60).slice(-2)
-            
-      ].join(':')
-}
